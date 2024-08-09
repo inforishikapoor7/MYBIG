bd3c53114a807a747262d26b2a087ae458332e5b
inforishikapoor7 <105081184+inforishikapoor7@users.noreply.github.com>
2024-08-09 16:20:40 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,16 @@
 terraform {
+  required_version = ">=1.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.84.0"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
-  required_version = ">= 1.1.3"
 }
 
 provider "azurerm" {

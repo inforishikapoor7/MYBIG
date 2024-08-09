b344e6402cc64bb219c18807ccfafd493d9207f5
inforishikapoor7 <105081184+inforishikapoor7@users.noreply.github.com>
2024-08-09 16:22:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 variable "resource_group_location" {
-  default     = "West Europe"
+  default     = "eastus"
   description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
 }
